12a68896e035a2729fe35b41dc2dca94809e5fe7
siva2022000 <sivamohan.mukkara@gmail.com>
2024-08-11 18:26:17 +0530

    edit and delete card changes

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,9 +1,29 @@
+import editImg from "../images/editImage.svg";
+import deleteImg from "../images/deleteImage.svg";
+
 function TaskShow({ task }) {
   return (
     <div className="card">
-      <div className="card-content">
-        <b>{task.time}</b>
-        <div>{task.details}</div>
+      <div className="card-content  pt-3 pb-2">
+        <nav className="level">
+          <div className="level-left ">
+            <div className="level-item">
+              <div className="container">
+                <div>{task.time}</div>
+                <div>
+                  <label className="checkbox">
+                    <input type="checkbox" />
+                    <span className="ml-2">
+                      <b>{task.details}</b>
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </nav>
       </div>
     </div>
   );
